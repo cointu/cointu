ae64c132e79a6b8f82d74f48f0e29dfcabe6518b
zdwolfe <wolfe.zach@gmail.com>
2014-01-21 18:15:37 -0500

    Assume yes for electrum

--- packages/electrum/install.sh
+++ packages/electrum/install.sh
@@ -1,5 +1,5 @@
 echo "Installing electrum dependencies.."
-sudo apt-get install python-qt4 python-pip python-slowaes 
+sudo apt-get install python-qt4 python-pip python-slowaes  --assume-yes
 echo "Installing electrum.."
 sudo pip install "https://download.electrum.org/Electrum-1.9.7.tar.gz#md5=5764f38d6e4bc287a577c8d16e797882" --quiet 
 which electrum 
