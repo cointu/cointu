44c234bcfb8e8c3c7a9ed70974a7299108fea78c
zdwolfe <wolfe.zach@gmail.com>
2014-01-24 16:58:34 -0500

    PeerGuardian auto-config. Fixes #35

--- packages/PeerGuardian/install.sh
+++ packages/PeerGuardian/install.sh
@@ -1,17 +1,15 @@
 sudo add-apt-repository ppa:jre-phoenix/ppa --yes > /dev/null
 sudo apt-get update > /dev/null 
-sudo apt-get install pglcmd pglgui --yes --force-yes
-which pgld
-if [ $? -eq 1 ]; then 
-  echo "Install of pgld failed"
-  exit 1;
+# Since PeerGuardian uses debconf to configure boot options post install
+# so set debconf to noninteractive
+sudo DEBIAN_FRONTEND=noninteractive apt-get -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' -f -q -y install pgld pglcmd pglgui
+which pgld > /dev/null
+if [ $? -ne 0 ] ; then
+  exit 1
 fi
-
-which pglcmd
-if [ $? -eq 1 ]; then 
-  echo "Install of pgld failed"
-  exit 1;
+which pglcmd > /dev/null
+if [ $? -ne 0 ] ; then
+  exit 1
 fi
-
-which pglcmd
+which pglgui > /dev/null
 exit $?
